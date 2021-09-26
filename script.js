8e969cc5989da51bf8f632f31b85a8dce87d2234
nikhilnitrr <nikhilchandrakar3@gmail.com>
2021-09-26 17:52:44 +0530

    banana talk

---
+++ script.js
@@ -0,0 +1,17 @@
+var btn = document.querySelector("#btn-primary");
+var input = document.querySelector("#input-txt");
+var output = document.querySelector("#output");
+var url = "https://api.funtranslations.com/translate/minion.json";
+
+function generateUrl(text) {
+  return url + "?text=" + text;
+}
+
+function myScript() {
+  fetch(generateUrl(input.value))
+    .then((response) => response.json())
+    .then((json) => (output.innerText = json.contents.translated))
+    .catch((e) => console.log(e));
+}
+
+btn.addEventListener("click", myScript);
